f9b2a02fb9a2886a241d067a597a6d84160b75ca
hangodek <140593449+hangodek@users.noreply.github.com>
2023-08-06 22:42:34 +0700

    Initial push

--- HackerRank/Nested List/Nested List.py
+++ HackerRank/Nested List/Nested List.py
@@ -1,17 +1,19 @@
+# Variation 2 (Advance)
+
 if __name__ == '__main__':
 
     score_list = []
     combined_list = []
 
-    for _ in range(int(input())):
+    for _ in range(int(input())): # To loop the code below as much as we put in to the input
         name = input()
         score = float(input())
 
         score_list.append(score)
-        combined_list.append([name, score])
+        combined_list.append([name, score]) # Name index 0, and score index 1
 
     score_list = sorted(score_list)
-    low_score = min(score_list)
+    low_score = min(score_list) # The lowest possible value from the score_list
     score_list_sorted = []
     combined_list_sorted = []
     
@@ -19,9 +21,9 @@ if __name__ == '__main__':
         if low_score != i:
             score_list_sorted.append(i)
     
-    for i in range(len(combined_list)):
-        if score_list_sorted[0] == combined_list[i][1]:
-            combined_list_sorted.append(combined_list[i])
+    for i in range(len(combined_list)):                         # Loop the code in range of as many values are in the list
+        if score_list_sorted[0] == combined_list[i][1]:         # The [i][1] mean the combined_list that in the i variable or i
+            combined_list_sorted.append(combined_list[i])       # 0 stand for the student name, and 1 stand for the score
 
     result = []
     for i in range(len(combined_list_sorted)):
@@ -31,12 +33,14 @@ if __name__ == '__main__':
         print(i)
 
 """
+# Variation 2 (A little easier)
+
 student_list = []
 
 for _ in range(int(input())):
     name = input()
     score = float(input())
-    student_list.append([name, score])
+    student_list.append([name, score]) # In this variation, we directly input the name and the score to the list
 
 student_list.sort(key=lambda x: x[1])
 
@@ -47,4 +51,4 @@ for name, score in sorted(student_list):
     if score == second_lowest_score:
         print(name)
 
-I still didn't understand about this """        
+I still didn't understand about the lambda """        
