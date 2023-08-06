a636df5d0574266ef61c01cd7821b7d67b2919c4
hangodek <140593449+hangodek@users.noreply.github.com>
2023-08-06 21:38:33 +0700

    Initial push

--- HackerRank/Find the Runner-Up Score!/Find the Runner-Up Score!.py
+++ HackerRank/Find the Runner-Up Score!/Find the Runner-Up Score!.py
@@ -1,4 +1,6 @@
 """ 
+# Varitaion 1 (A little complex)
+
 if __name__ == '__main__':
      score = []
     n = int(input())
@@ -9,7 +11,7 @@ if __name__ == '__main__':
 
     runner_up_score = None
     for score in sorted_score:
-        if score < max(sorted_score):
+        if score < max(sorted_score): 
             runner_up_score = sorted_score
             break
 
@@ -23,12 +25,12 @@ if __name__ == '__main__':
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
-    score = list(set(arr))
-    score.sort(reverse = True)
+    score = list(set(arr)) # We use this command to turn the arr to set (Removing all the duplicated number) and turning it back to a list
+    score.sort(reverse = True)  # .sort is as same as sorted but a little simple    # so we get a clean free list without any duplicate number
     score.append(n)
     
     
-    if len(score) >= 2:
+    if len(score) >= 2: # if score has 2 or equal item inside it, then the print index 1 of the score list will be executed
         print(score[1])
     else:
         print(None)
